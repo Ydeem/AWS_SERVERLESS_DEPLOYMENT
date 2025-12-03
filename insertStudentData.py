12d850d7bd51f6a8515912b1663e092a2a55ea6e
Ydeem <davidkwilson271@gmail.com>
2025-12-03 13:59:44 +0000

    Update insertStudentData.py

--- insertStudentData.py
+++ insertStudentData.py
@@ -10,17 +10,17 @@ table = dynamodb.Table('studentData')
 def lambda_handler(event, context):
     # Extract values from the event object we got from the Lambda service and store in variables
     student_id = event['studentid']
-    name = event['name']
-    student_class = event['class']
-    age = event['age']
+    name = event['studentName']
+    student_class = event['studentClass']
+    age = event['studentAge']
     
     # Write student data to the DynamoDB table and save the response in a variable
     response = table.put_item(
         Item={
             'studentid': student_id,
-            'name': name,
-            'class': student_class,
-            'age': age
+            'studentName': name,
+            'studentClass': student_class,
+            'studentAge': age
         }
     )
     
